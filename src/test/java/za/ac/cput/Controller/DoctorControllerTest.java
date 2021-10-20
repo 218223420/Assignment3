e9c4a69aec075ffe0d2616231d6e92e3f6b99785
BHEKA GUMEDE <218223420@mycput.ac.za>
2021-10-20 08:34:25 +0200

    Update

---
+++ src/test/java/za/ac/cput/Controller/DoctorControllerTest.java
@@ -0,0 +1,84 @@
+package za.ac.cput.Controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.cput.Entity.Doctor;
+import za.ac.cput.Factory.DoctorFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+ class DoctorControllerTest {
+    private static Doctor doctor = DoctorFactory.createDoctor("Peace", "Smith", 48000.00);
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private final String baseURL = "http://localhost:8080/doctor";
+
+    @Test
+    void a_create()
+    {
+        String url = baseURL + "/create";
+        ResponseEntity<Doctor> postResponse = restTemplate.postForEntity(url, doctor, Doctor.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+        assertEquals(postResponse.getStatusCode(), HttpStatus.OK);
+        doctor = postResponse.getBody();
+        System.out.println("Saved data: " + doctor);
+        assertEquals(doctor.getDoctorID(), postResponse.getBody().getDoctorID());
+    }
+
+    @Test
+    void b_read() {
+        String  url = baseURL + "/read/" + doctor.getFirstName();
+        System.out.println("URL: " + url);
+        ResponseEntity<Doctor> response = restTemplate.getForEntity(url, Doctor.class);
+        assertEquals(doctor.getFirstName(), response.getBody().getFirstName());
+
+        //System.out.println("Read: " + read);
+    }
+
+    @Test
+    void c_update()
+    {
+        Doctor updated = new Doctor.Builder().copy(doctor).setFirstName("Lucky").build();
+        String url = baseURL + "/update";
+        System.out.println("URL: " + url);
+        System.out.println("Post data: " + updated);
+        ResponseEntity<Doctor> response = restTemplate.postForEntity(url, updated, Doctor.class);
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void d_delete()
+    {
+        String url = baseURL + "/delete" + doctor.getDoctorID();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
+    }
+
+
+    @Test
+    void e_getAll()
+    {
+        String url = baseURL + "/getall";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println("Show all:");
+        System.out.println(response);
+        System.out.println(response.getBody());
+
+    }
+
+
+}
